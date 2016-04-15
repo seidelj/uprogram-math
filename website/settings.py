39df250880d3c9a088989ad3579355d407f52b47
Joseph <seidel.jp@gmail.com>
2016-04-15 09:37:18 -0500

    debuggin api

--- website/settings.py
+++ website/settings.py
@@ -51,9 +51,9 @@ INSTALLED_APPS = (
 )
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    #'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     "PAGE_SIZE": 100,
-    "DEFAULT_AUTHENTICATION_CLASSES": ('rest_framework.authentication.TokenAuthentication',),
+    #"DEFAULT_AUTHENTICATION_CLASSES": ('rest_framework.authentication.TokenAuthentication',),
 }
 
 SITE_ID = 1
